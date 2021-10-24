bfc26df121f3b515edbbf2ee82032c873dacecb6
Roland Käsi <rkasi@tktk.ee>
2021-10-24 16:15:30 +0300

    add new projects

---
+++ FridayNightRandomReturn/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FridayNightRandomReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomFood, randomDrink, randomMovie;
+            string[] foods = { "chicken wings", "pizza", "sushi", "burger", "kebab" };
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite", "water" };
+            string[] movies = { "Grey's Anatomy", "Friends", "James Bond No Time to Die", "Soo", "O2" };
+
+            randomFood = PickRandomElementFromArray(foods);
+            randomDrink = PickRandomElementFromArray(drinks);
+            randomMovie = PickRandomElementFromArray(movies);
+
+            Console.WriteLine($"Tonight you will have some {randomFood} with {randomDrink} and watch {randomMovie}");
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string PickRandomElementFromArray(string[] someArray)
+        {
+            string randomElement = someArray[GenerateRandomIndex(someArray)];
+            return randomElement;
+        }
+    }
+}
